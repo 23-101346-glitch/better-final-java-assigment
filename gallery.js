80dbbc59a25c6728e7775876d4ef00d2d7c85395
Janna khaled <23-101346@students.eui.edu.eg>
2025-12-22 19:41:48 +0200

    footer and dark mode

--- gallery.js
+++ gallery.js
@@ -1,3 +1,23 @@
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+});
+
+window.addEventListener("load", () => {
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+});
+
 const slides = document.querySelectorAll('.slide');
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
