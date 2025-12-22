3a1ab6e93460617270af8c0a1297e53ce6e2d515
Janna khaled <23-101346@students.eui.edu.eg>
2025-12-22 23:07:51 +0200

    footer and dark mode

--- about.js
+++ about.js
@@ -10,3 +10,15 @@ function toggleReadMore(btn) {
         btn.textContent = "Read less";
     }
 }
+
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+});
